e743fc230391f52eec26ee98e3dd00ed6f9693e2
DuncanBiwott <bduncan19@student.kibu.ac.ke>
2024-01-23 17:23:54 +0300

    fixed extraction issue

--- PdfPlamberExtraction.py
+++ PdfPlamberExtraction.py
@@ -17,6 +17,18 @@ def extract_data(pdf_path):
         each_page_data = pd.DataFrame(table[1:], columns=df.columns)
         # Adding each df to main df by using concat (Eg :sum of numbers in array)
         df = pd.concat([df, each_page_data], ignore_index=True)
-    return df.to_csv('/home/dbiwott/Downloads/extracted_data.csv', index=False)
+        # df.to_csv('/home/dbiwott/Downloads/extracted_data.csv', index=False)
+    return df.to_csv('/home/dbiwott/Downloads/rotated.csv', index=False)
 
-extract_data('/home/dbiwott/Downloads/tabithaaccountstatementrafiki-2.pdf')
+# extract_data('/home/dbiwott/Downloads/tabithaaccountstatementrafiki-2.pdf')
+# extract_data("/home/dbiwott/Downloads/rotated.pdf")
+
+my_data=pd.read_csv('/home/dbiwott/Downloads/rotated.csv')
+print(my_data)
+my_data.loc[4,"Credit"]=5000.00
+print(my_data.loc[4,"Credit"])
+print(my_data.iloc[4,6])
+print(my_data.groupby(my_data["Credit"]))
+
+
+
